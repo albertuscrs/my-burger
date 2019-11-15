14f41f8a1441022ed70a88eb34960f0c30d2df55
albertuscrs <achristyadhi@gmail.com>
2019-11-15 11:11:14 +0700

    Adding: axios to react and connect it to firebase free server using real-time database store; Superb

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -79,7 +80,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You can continue');
+    // alert('You can continue');
+    var date = new Date().toLocaleString('id');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, //calculate in backend
+      customer: {
+        name: 'Someone in ' + date,
+        address: {
+          street: 'test test',
+          zipCode: '1234',
+          country: 'neverland'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'Insane'
+    }
+    axios.post('/orders.json', order).then(response => console.log(response))
+         .catch(error => console.log(error));
   }
 
   render () {
